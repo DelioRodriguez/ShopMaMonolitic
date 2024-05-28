314b19c534332b988d06b78dff5dd5071d8e48a1
Dayner <164125521+Dayner-Garcia@users.noreply.github.com>
2024-05-28 17:55:01 -0400

    first commit Dayner

--- ShopMaMonolitic/obj/Release/net8.0/ShopMaMonolitic.AssemblyInfo.cs
+++ ShopMaMonolitic/obj/Release/net8.0/ShopMaMonolitic.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopMaMonolitic")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e3a3bb127f632fd8b5ff0f0981a36c21119f462")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopMaMonolitic")]
