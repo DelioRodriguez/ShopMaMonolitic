c129ab2de2c81192b1201b35181c6f874e8266a7
Dayner <164125521+Dayner-Garcia@users.noreply.github.com>
2024-05-28 18:26:39 -0400

    OK

---
+++ ShopMaMonolitic/Data/Entities/SalesOrderDetails.cs
@@ -0,0 +1,12 @@
+﻿namespace ShopMaMonolitic.Data.Core;
+
+public class SalesOrderDetails : BaseEntity
+{
+    public int qty { get; set; }
+    public int discount { get; set; }
+    public SalesOrderDetails()
+    {
+        
+    }
+    
+}
