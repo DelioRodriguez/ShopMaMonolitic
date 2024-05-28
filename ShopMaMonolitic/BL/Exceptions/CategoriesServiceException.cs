47ac8acb6b1ecd7f98a916da34d75d30baf68287
Deliorodriguez28@outlook.com <deliolorenzo28@gmail.com>
2024-05-28 17:42:42 -0400

    Update a BL Exceptions

--- ShopMaMonolitic/BL/Exceptions/CategoriesServiceException.cs
+++ ShopMaMonolitic/BL/Exceptions/CategoriesServiceException.cs
@@ -5,6 +5,9 @@ public class CategoriesServiceException : Exceptions
     
     public CategoriesServiceException(string message) : base(message)
     {
+
+       public CategoriesServiceException(string message) : base (message){
+ }
             
     }
 }
